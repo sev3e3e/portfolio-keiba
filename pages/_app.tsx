90a3f4776e846022b9b1b2a898e26094b77a0f66
medilore <fc2tokaqq+medilore@gmail.com>
2022-11-24 13:59:28 +0900

    [feat]Add authentication using NextAuth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,46 @@
 // import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
-import { NextUIProvider } from "@nextui-org/react";
+import { Button, Link, Navbar, NextUIProvider, Text } from "@nextui-org/react";
+import { SessionContextValue, SessionProvider } from "next-auth/react";
+import { useSession, signIn, signOut } from "next-auth/react";
 
-export default function App({ Component, pageProps }: AppProps) {
+export const NavBarUserItem = () => {
+    const { data: session, status } = useSession();
+    if (status === "unauthenticated") {
+        return (
+            <Button auto flat as={Link} href="/api/auth/signin/google">
+                Log in
+            </Button>
+        );
+    } else {
+        return (
+            <>
+                <Text>Logged in as</Text>
+                <Text>{session?.user?.name}</Text>
+            </>
+        );
+    }
+};
+
+export default function App({
+    Component,
+    pageProps: { session, ...pageProps },
+}: AppProps) {
+    console.log(session);
     return (
-        <NextUIProvider>
-            <Component {...pageProps} />
-        </NextUIProvider>
+        <SessionProvider session={session}>
+            <NextUIProvider>
+                <Navbar isBordered variant="sticky">
+                    <Navbar.Brand>Portfolio-netkeiba</Navbar.Brand>
+                    <Navbar.Content>
+                        <Navbar.Item>
+                            <NavBarUserItem />
+                        </Navbar.Item>
+                    </Navbar.Content>
+                </Navbar>
+                <Component {...pageProps} />
+            </NextUIProvider>
+        </SessionProvider>
     );
 }
--- prisma/scheme.md
+++ prisma/scheme.md
@@ -63,9 +63,51 @@ erDiagram
     Int favorite  
     }
   
+
+  Account {
+    String id PK 
+    String type  
+    String provider  
+    String providerAccountId  
+    String refresh_token  "nullable"
+    String access_token  "nullable"
+    Int expires_at  "nullable"
+    String token_type  "nullable"
+    String scope  "nullable"
+    String id_token  "nullable"
+    String session_state  "nullable"
+    String oauth_token_secret  "nullable"
+    String oauth_token  "nullable"
+    }
+  
+
+  Session {
+    String id PK 
+    String sessionToken  
+    DateTime expires  
+    }
+  
+
+  User {
+    String id PK 
+    String name  "nullable"
+    String email  "nullable"
+    DateTime emailVerified  "nullable"
+    String image  "nullable"
+    }
+  
+
+  VerificationToken {
+    String identifier  
+    String token  
+    DateTime expires  
+    }
+  
     HorseRecord o{--|| Horse : "horse"
     JockeyRecord o{--|| Jockey : "jockey"
     Race_DetailHorse o{--|| Race : "race"
     Race_DetailHorse o{--|| Horse : "horse"
     Race_DetailHorse o{--|| Jockey : "jockey"
+    Account o{--|| User : "user"
+    Session o{--|| User : "user"
 ```
